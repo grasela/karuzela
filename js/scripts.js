8be186f0b1d2ddeb6fc51099d7af6bb5638f3704
Eryk Grasela <grasela.eryk@gmail.com>
2018-01-13 15:17:33 +1100

    carouel and next prev buttons done

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+$(function(){
+	 var carouselList = $("#carousel ul");
+	 var changeSlides = setInterval(function(){
+    	 carouselList.animate(
+    		 {
+    		 'marginLeft':-400}, 500, function(moveFirstSlide){
+    			 var firstItem = carouselList.find("li:first");
+				 var lastItem = carouselList.find("li:last");  
+				 var moveFirstSlide = lastItem.after(firstItem);
+				 $('carouselList').css ({
+					 marginLeft: 0
+				 });
+    		 });
+ }, 3000);
+	 $('.next').on('click', function(){
+		carouselList.animate(
+    		 {
+    		 'marginRight': '400px'}, 0, function(moveFirstSlide){
+    			 var firstItem = carouselList.find("li:first");
+				 var lastItem = carouselList.find("li:last");  
+				 var moveFirstSlide = lastItem.after(firstItem);
+				 $('carouselList').css ({
+					 marginLeft: 0
+				 });
+    		 });
+    
+	});
+	$('.prev').on('click', function(){
+		carouselList.animate(
+    		 {
+    		 'marginLeft': '-400px'}, 0, function(moveLastSlide){
+    			 var firstItem = carouselList.find("li:first");
+				 var lastItem = carouselList.find("li:last");  
+				 var moveLastSlide = firstItem.after(lastItem);
+				 $('carouselList').css ({
+					 marginLeft: 0
+				 });
+    		 });
+	});
+	$('.dot1').on('click', function() {
+		 
+	});
+
+});
