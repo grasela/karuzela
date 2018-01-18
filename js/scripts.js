2c38af390c55d7e74fbe6eaecbebbbd6ba7ba4ca
Eryk Grasela <grasela.eryk@gmail.com>
2018-01-18 20:52:35 +1100

    fixed the dots still mismatching after first full round

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,67 @@
 $(function(){
 	 var carouselList = $("#carousel ul");
-	 var changeSlides = setInterval(function(){
-    	 carouselList.animate(
-    		 {
-    		 'marginLeft':-400}, 500, function(moveFirstSlide){
-    			 var firstItem = carouselList.find("li:first");
-				 var lastItem = carouselList.find("li:last");  
-				 var moveFirstSlide = lastItem.after(firstItem);
-				 $('carouselList').css ({
-					 marginLeft: 0
-				 });
-    		 });
- }, 3000);
-	 $('.next').on('click', function(){
-		carouselList.animate(
-    		 {
-    		 'marginRight': '400px'}, 0, function(moveFirstSlide){
-    			 var firstItem = carouselList.find("li:first");
-				 var lastItem = carouselList.find("li:last");  
-				 var moveFirstSlide = lastItem.after(firstItem);
-				 $('carouselList').css ({
-					 marginLeft: 0
-				 });
-    		 });
-    
-	});
+	 var changeSlides = setInterval(changeSlide, 3000);
+	
+	function changeSlide(direction){
+		var animatedStyles = {
+			marginRight: '400px',
+		}
+		if (direction === 'left') {
+			animatedStyles = {
+				marginLeft: '-400px',
+			}
+		}
+	
+		carouselList.animate(animatedStyles, 500, function(){
+			moveSlide(direction);
+		});
+
+	}
+	function moveSlide(direction){
+		 var firstItem = carouselList.find("li:first");
+		 var lastItem = carouselList.find("li:last"); 
+		 if (direction === 'left') {
+		 	firstItem.before(lastItem);
+		 } else {
+		 	lastItem.after(firstItem);
+		 }
+		 $('carouselList').css ({
+			 marginLeft: 0
+		 });
+	}
 	$('.prev').on('click', function(){
-		carouselList.animate(
-    		 {
-    		 'marginLeft': '-400px'}, 0, function(moveLastSlide){
-    			 var firstItem = carouselList.find("li:first");
-				 var lastItem = carouselList.find("li:last");  
-				 var moveLastSlide = firstItem.after(lastItem);
-				 $('carouselList').css ({
-					 marginLeft: 0
-				 });
-    		 });
+		clearInterval(changeSlides);
+		changeSlide('left');
+		changeSlides = setInterval(changeSlide, 3000);
 	});
-	$('.dot1').on('click', function() {
-		 
+
+
+	$('.next').on('click', function(){
+		clearInterval(changeSlides);
+		changeSlide('right');
+		changeSlides = setInterval(changeSlide, 3000);
 	});
+	$('.dot').on('click', function() {
+		clearInterval(changeSlides);
+		var currentSlide = carouselList.find("li:first").data("slide");
+		var currentDot = $(this).index() +1;
+		if (currentDot < currentSlide) {
+			for (i = currentSlide; i >= currentDot; i--) {
+			changeSlide('left');
+			};
+		} else {
+			for (i = currentSlide; i < currentDot; i++){
+				changeSlide('right');
+			}
+		}
+	});
+		
 });
+
+		 	
+
+
+	 
+				
+	
+
